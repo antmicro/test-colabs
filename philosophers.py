cfb1b71f409a7e567663debd4c01e3db02a343d3
Mateusz Holenko <mholenko@antmicro.com>
2022-04-20 17:05:18 +0200

    Add colab for the philosophers sample

---
+++ philosophers.py
@@ -0,0 +1,59 @@
+# %% [markdown]
+"""
+![Renode](https://dl.antmicro.com/projects/renode/renode.svg)
+<table align="left">
+  <td>
+    <a target="_blank" href="https://colab.research.google.com/github/antmicro/test-colabs/blob/main/philosophers.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-in-colab.png" />Run in Google Colab</a>
+  </td>
+  <td>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/philosophers.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-ipynb.png" />View ipynb on GitHub</a>
+  </td>
+  <td>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/philosophers.py"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-source.png" />View Python source on GitHub</a>
+  </td>
+</table>
+"""
+
+# %% [markdown]
+"""
+## Install requirements
+"""
+
+# %%
+! pip install -q git+https://github.com/antmicro/renode-colab-tools.git
+! pip install -q git+https://github.com/antmicro/renode-run.git
+
+# %% [markdown]
+"""## Run the philosophers example in Renode"""
+
+# %%
+! renode-run demo --board hifive1_revb philosophers -- --console -e 'machine EnableProfiler @/tmp/metrics.dump; emulation RunFor "0.3"; q'
+
+# %% [markdown]
+"""## Renode metrics analysis"""
+
+# %%
+import sys
+from pathlib import Path
+sys.path.append(Path('/root/.config/renode/renode-run.path').read_text())
+
+from renode_colab_tools import metrics
+from tools.metrics_analyzer.metrics_parser import MetricsParser
+metrics.init_notebook_mode(connected=False)
+parser = MetricsParser('/tmp/metrics.dump')
+
+# %%
+metrics.configure_plotly_browser_state()
+metrics.show_executed_instructions(parser)
+
+# %%
+metrics.configure_plotly_browser_state()
+metrics.show_memory_access(parser)
+
+# %%
+metrics.configure_plotly_browser_state()
+metrics.show_exceptions(parser)
+
+# %%
+metrics.configure_plotly_browser_state()
+metrics.show_peripheral_access(parser)
