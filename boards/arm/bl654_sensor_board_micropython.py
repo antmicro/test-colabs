b656e70e207cfe8fb98271e1226dd2870bf3a7e4
RenodeBot <renode-bot@antmicro.com>
2022-05-16 22:43:07 +0000

    Automatic update of ipynb files
    Signed-off-by: RenodeBot <renode-bot@antmicro.com>

--- boards/arm/bl654_sensor_board_micropython.py
+++ boards/arm/bl654_sensor_board_micropython.py
@@ -3,13 +3,13 @@
 ![Renode](https://dl.antmicro.com/projects/renode/renode.svg)
 <table align="left">
   <td>
-    <a target="_blank" href="https://colab.research.google.com/github/antmicro/test-colabs/blob/main/boards/arm/bl654_sensor_board-micropython.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-in-colab.png" />Run in Google Colab</a>
+    <a target="_blank" href="https://colab.research.google.com/github/antmicro/test-colabs/blob/main/boards/arm/bl654_sensor_board_micropython.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-in-colab.png" />Run in Google Colab</a>
   </td>
   <td>
-    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/arm/bl654_sensor_board-micropython.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-ipynb.png" />View ipynb on GitHub</a>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/arm/bl654_sensor_board_micropython.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-ipynb.png" />View ipynb on GitHub</a>
   </td>
   <td>
-    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/arm/bl654_sensor_board-micropython.py"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-source.png" />View Python source on GitHub</a>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/arm/bl654_sensor_board_micropython.py"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-source.png" />View Python source on GitHub</a>
   </td>
 </table>
 """
