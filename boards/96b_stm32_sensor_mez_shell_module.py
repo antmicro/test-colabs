d7df3db877eb7c7f63806afcd07b929fa1577f23
RenodeBot <renode-bot@antmicro.com>
2022-05-17 14:28:31 +0000

    Automatic update of ipynb files
    Signed-off-by: RenodeBot <renode-bot@antmicro.com>

---
+++ boards/96b_stm32_sensor_mez_shell_module.py
@@ -0,0 +1,91 @@
+# %% [markdown]
+"""
+![Renode](https://dl.antmicro.com/projects/renode/renode.svg)
+<table align="left">
+  <td>
+    <a target="_blank" href="https://colab.research.google.com/github/antmicro/test-colabs/blob/main/boards/96b_stm32_sensor_mez_shell_module.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-in-colab.png" />Run in Google Colab</a>
+  </td>
+  <td>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/96b_stm32_sensor_mez_shell_module.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-ipynb.png" />View ipynb on GitHub</a>
+  </td>
+  <td>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/96b_stm32_sensor_mez_shell_module.py"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-source.png" />View Python source on GitHub</a>
+  </td>
+</table>
+"""
+
+# %% [markdown]
+"""
+## Install requirements
+"""
+
+# %%
+! pip install -q git+https://github.com/antmicro/renode-colab-tools.git@tabbed-metrics
+! pip install -q git+https://github.com/antmicro/renode-run.git@new-features
+! pip install -q git+https://github.com/antmicro/pyrenode.git@renode-run-experiments
+! pip install -q robotframework==4.0.1
+! renode-run download
+
+# %% [markdown]
+"""## Start Renode"""
+
+# %%
+from pyrenode import connect_renode, get_keywords
+connect_renode()
+get_keywords()
+
+# %% [markdown]
+"""## Setup a script"""
+
+# %%
+%%writefile script.resc
+
+using sysbus
+$name?="96b_stm32_sensor_mez"
+mach create $name
+
+machine LoadPlatformDescription @https://zephyr-dashboard.renode.io/96b_stm32_sensor_mez-shell_module.repl
+machine EnableProfiler $ORIGIN/metrics.dump
+
+showAnalyzer sysbus.uart4
+
+macro reset
+"""
+    sysbus LoadELF @https://zephyr-dashboard.renode.io/96b_stm32_sensor_mez-zephyr-shell_module.elf
+    cpu0 VectorTableOffset `sysbus GetSymbolAddress "_vector_table"`
+"""
+
+runMacro $reset
+
+# %% [markdown]
+"""## Run the sample"""
+
+# %%
+ExecuteCommand("include @script.resc")
+CreateTerminalTester("sysbus.uart4", timeout=5)
+StartEmulation()
+
+WaitForPromptOnUart("uart:~$")
+WriteLineToUart("")
+WaitForPromptOnUart("uart:~$")
+WriteLineToUart("demo board")
+WaitForLineOnUart("96b_stm32_sensor_mez")
+
+print(ExecuteCommand("sysbus.uart4 DumpHistoryBuffer"))
+
+ResetEmulation()
+# %% [markdown]
+"""## Renode metrics analysis"""
+
+# %%
+import sys
+from pathlib import Path
+sys.path.append(Path('/root/.config/renode/renode-run.path').read_text())
+
+from renode_colab_tools import metrics
+from tools.metrics_analyzer.metrics_parser import MetricsParser
+metrics.init_notebook_mode(connected=False)
+parser = MetricsParser('metrics.dump')
+
+# %%
+metrics.display_metrics(parser)
---
+++ boards/nrf51_ble400_tensorflow_lite_micro.py
@@ -0,0 +1,90 @@
+# %% [markdown]
+"""
+![Renode](https://dl.antmicro.com/projects/renode/renode.svg)
+<table align="left">
+  <td>
+    <a target="_blank" href="https://colab.research.google.com/github/antmicro/test-colabs/blob/main/boards/nrf51_ble400_tensorflow_lite_micro.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-in-colab.png" />Run in Google Colab</a>
+  </td>
+  <td>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/nrf51_ble400_tensorflow_lite_micro.ipynb"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-ipynb.png" />View ipynb on GitHub</a>
+  </td>
+  <td>
+    <a target="_blank" href="https://github.com/antmicro/test-colabs/blob/main/boards/nrf51_ble400_tensorflow_lite_micro.py"><img src="https://raw.githubusercontent.com/antmicro/test-colabs/main/.static/view-source.png" />View Python source on GitHub</a>
+  </td>
+</table>
+"""
+
+# %% [markdown]
+"""
+## Install requirements
+"""
+
+# %%
+! pip install -q git+https://github.com/antmicro/renode-colab-tools.git@tabbed-metrics
+! pip install -q git+https://github.com/antmicro/renode-run.git@new-features
+! pip install -q git+https://github.com/antmicro/pyrenode.git@renode-run-experiments
+! pip install -q robotframework==4.0.1
+! renode-run download
+
+# %% [markdown]
+"""## Start Renode"""
+
+# %%
+from pyrenode import connect_renode, get_keywords
+connect_renode()
+get_keywords()
+
+# %% [markdown]
+"""## Setup a script"""
+
+# %%
+%%writefile script.resc
+
+using sysbus
+$name?="nrf51_ble400"
+mach create $name
+
+machine LoadPlatformDescription @https://zephyr-dashboard.renode.io/nrf51_ble400-tensorflow_lite_micro.repl
+machine EnableProfiler $ORIGIN/metrics.dump
+
+showAnalyzer sysbus.uart0
+
+macro reset
+"""
+    sysbus LoadELF @https://zephyr-dashboard.renode.io/nrf51_ble400-zephyr-tensorflow_lite_micro.elf
+    cpu0 VectorTableOffset `sysbus GetSymbolAddress "_vector_table"`
+"""
+
+runMacro $reset
+
+# %% [markdown]
+"""## Run the sample"""
+
+# %%
+ExecuteCommand("include @script.resc")
+CreateTerminalTester("sysbus.uart0", timeout=15)
+StartEmulation()
+
+WaitForLineOnUart("x_value: .* y_value: .*", treatAsRegex=True)
+WaitForLineOnUart("x_value: .* y_value: .*", treatAsRegex=True)
+WaitForLineOnUart("x_value: .* y_value: .*", treatAsRegex=True)
+WaitForLineOnUart("x_value: .* y_value: .*", treatAsRegex=True)
+
+print(ExecuteCommand("sysbus.uart0 DumpHistoryBuffer"))
+
+ResetEmulation()
+# %% [markdown]
+"""## Renode metrics analysis"""
+
+# %%
+import sys
+from pathlib import Path
+sys.path.append(Path('/root/.config/renode/renode-run.path').read_text())
+
+from renode_colab_tools import metrics
+from tools.metrics_analyzer.metrics_parser import MetricsParser
+metrics.init_notebook_mode(connected=False)
+parser = MetricsParser('metrics.dump')
+
+# %%
+metrics.display_metrics(parser)
