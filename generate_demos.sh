574e52c15bc5be2e17b7c840f48cc7be0ee09e71
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2023-03-07 13:46:31 +0100

    Add blinky demo

--- generate_demos.sh
+++ generate_demos.sh
@@ -2,7 +2,7 @@
 
 mkdir -p boards/
 
-demos=(philosophers hello_world tensorflow_lite_micro shell_module micropython)
+demos=(philosophers hello_world tensorflow_lite_micro shell_module micropython blinky)
 SCRIPT="cpu0 VectorTableOffset \`sysbus GetSymbolAddress \"_vector_table\"\`"
 
 for i in "${!demos[@]}"
@@ -12,13 +12,14 @@ do
     board_names=( $(jq -r ' .[] | select(.status | contains("PASSED")) | .board_name' /tmp/results-"${demos[i]}"-all.json))
     board_path=( $(jq -r ' .[] | select(.status | contains("PASSED")) | .board_path' /tmp/results-"${demos[i]}"-all.json))
     uart_names=( $(jq -r ' .[] | select(.status | contains("PASSED")) | .uart_name' /tmp/results-"${demos[i]}"-all.json))
+    gpio_led_names=( $(jq -r ' .[] | select(.status | contains("PASSED")) | .peripherals["gpio-led"] | (.name + "." + .led_name)' /tmp/results-"${demos[i]}"-all.json))
     readarray -t cpus < <(jq -r ' .[] | select(.status | contains("PASSED")) | .cpu' /tmp/results-"${demos[i]}"-all.json)
     for j in "${!board_path[@]}"
     do
         echo "${demos[i]} ${board_names[j]}"
         board_path="boards/${board_names[j]}"
         # First we replace Jinja templates in the demo-specific input. We'll use it later to fill out the Python template file
-        jinja -D zephyr_platform ${board_names[j]} -D uart_name ${uart_names[j]} -o "/tmp/${board_names[j]}_${demos[i]}" ${demos[i]}
+        jinja -D zephyr_platform ${board_names[j]} -D uart_name ${uart_names[j]} -D gpio_led_name ${gpio_led_names[j]} -o "/tmp/${board_names[j]}_${demos[i]}" ${demos[i]}
         sample=`cat /tmp/${board_names[j]}_${demos[i]}`
         if echo ${cpus[j]##*-> } | grep -q 'arm/armv.-m' ; then
             # For ARM Cortex-M platforms we explicitly initialize VTOR in the script, as the OS build system can place it in various parts of the binary.
