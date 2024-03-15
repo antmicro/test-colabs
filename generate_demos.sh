6eb2cd99846e0fdf33077a4957c13f52e39c360d
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-03-15 13:06:59 +0100

    [#48645] Add uboot

--- generate_demos.sh
+++ generate_demos.sh
@@ -5,6 +5,9 @@ mkdir -p boards/
 zephyr_DEMOS=(philosophers hello_world tensorflow_lite_micro shell_module micropython blinky hello_world_user synchronization lz4 rust-app)
 zephyr_DASHBOARD=https://zephyr-dashboard.renode.io
 zephyr_VERSION="$(curl -s $zephyr_DASHBOARD/zephyr.version)"
+uboot_DEMOS=(uboot)
+uboot_DASHBOARD=https://u-boot-dashboard.renode.io
+uboot_VERSION="$(curl -s $uboot_DASHBOARD/uboot.version)"
 
 SCRIPT="cpu0 VectorTableOffset \`sysbus GetSymbolAddress \"_vector_table\"\`"
 
@@ -27,11 +30,15 @@ generate_demos() {
             echo "${demo} ${platform}"
             board_path="boards/${platform}"
             repl="${!dashboard}/${platform}-${demo}/${platform}-${demo}.repl"
-            elf="https://new-zephyr-dashboard.renode.io/zephyr/${!version}/${platform}/${demo}/${demo}.elf"
+            elf="${!dashboard}/${platform}-${demo}/${platform}-${demo}.elf"
+            # On the Zephyr dashboard, binaries are stored separately
+            if [ "$1" = zephyr ]; then
+                elf="https://new-zephyr-dashboard.renode.io/zephyr/${!version}/${platform}/${demo}/${demo}.elf"
+            fi
             # First we replace Jinja templates in the demo-specific input. We'll use it later to fill out the Python template file
             jinja -D platform ${platform} -D uart_name ${uart_names[j]} -D gpio_led_name ${gpio_led_names[j]} -D software_version "${!version}" -o "/tmp/${platform}_${demo}" ${demo}
             sample=`cat /tmp/${platform}_${demo}`
-            if echo ${cpus[j]##*-> } | grep -q 'arm/armv.-m' ; then
+            if [ "$1" = zephyr ] && echo ${cpus[j]##*-> } | grep -q 'arm/armv.-m' ; then
                 # For ARM Cortex-M platforms we explicitly initialize VTOR in the script, as the OS build system can place it in various parts of the binary.
                 jinja -D sample_name ${demo} -D sample "${sample}" -D platform ${platform} -D board_path ${board_path} -D uart_name ${uart_names[j]} -D script "$SCRIPT" -D software_version "${!version}" -D repl "$repl" -D elf "$elf" -o "${board_path}"_"${demo}".py template.py
             else
@@ -43,3 +50,4 @@ generate_demos() {
 }
 
 generate_demos zephyr
+generate_demos uboot
