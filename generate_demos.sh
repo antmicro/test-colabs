a510bcf083321ddb07d7bf51d7115f0bda3b120d
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-04-02 17:20:38 +0200

    [#57400] Download from and link to the new Zephyr dashboard

--- generate_demos.sh
+++ generate_demos.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 mkdir -p boards/
 
 zephyr_DEMOS=(philosophers hello_world tensorflow_lite_micro shell_module micropython blinky hello_world_user synchronization lz4 rust-app kenning-zephyr-runtime-microtvm kenning-zephyr-runtime-tflitemicro)
-zephyr_DASHBOARD=https://zephyr-dashboard.renode.io
-zephyr_VERSION="$(curl -s $zephyr_DASHBOARD/zephyr.version)"
+zephyr_dashboard_base=https://new-zephyr-dashboard.renode.io
+zephyr_VERSION="$(curl -s $zephyr_dashboard_base/zephyr_sim/latest)"
+zephyr_renode_version="$(curl -s $zephyr_dashboard_base/zephyr_sim/$zephyr_VERSION/latest)"
+zephyr_DASHBOARD="$zephyr_dashboard_base/zephyr_sim/$zephyr_VERSION/$zephyr_renode_version"
 uboot_DEMOS=(uboot)
 uboot_DASHBOARD=https://u-boot-dashboard.renode.io
 uboot_VERSION="$(curl -s $uboot_DASHBOARD/uboot.version)"
@@ -28,7 +30,10 @@ generate_demos() {
     version="${1}_VERSION"
     # Get Renode platform description files for all boards (and demos)
     mkdir "$tmp/$1"
-    wget "${!dashboard}"/replkit.tar.xz -O - | tar xJ -C "$tmp/$1"
+    wget -P "$tmp" "${!dashboard}"/renodekit.tar.xz
+    # Assumption: no two boards with different architectures will have the same name
+    tar xf "$tmp/renodekit.tar.xz" -C "$tmp/$1" --wildcards '*.repl' --transform 's:.*/::g'
+    rm "$tmp/renodekit.tar.xz"
     for demo in "${!demos}"
     do
         # The JSON file created by Dashboard contains information on the board running the specified demo, test results, used serial port etc.
@@ -45,13 +50,16 @@ generate_demos() {
             platform_original="${original_names[j]}"
             echo "${demo} ${platform}"
             # Generate a script that halts every CPU except cpu0, if any are present
-            script="$(grep -Po '^.*(?=:\s+CPU\.)' "$tmp/${1}/$platform-$demo.repl" | { grep -Fxv cpu0 || :; } | while read -r c; do echo "$c IsHalted true"; done)"
+            repl_path="$tmp/${1}/$platform.repl"
+            script="$(grep -Po '^.*(?=:\s+CPU\.)' "$repl_path" | { grep -Fxv cpu0 || :; } | while read -r c; do echo "$c IsHalted true"; done)"
             board_path="boards/${platform}"
             repl="${!dashboard}/${platform}-${demo}/${platform}-${demo}.repl"
             elf="${!dashboard}/${platform}-${demo}/${platform}-${demo}.elf"
-            # On the Zephyr dashboard, binaries are stored separately
+            # On the Zephyr dashboard, the locations are different
             if [ "$1" = zephyr ]; then
-                elf="https://new-zephyr-dashboard.renode.io/zephyr/${!version}/${platform}/${demo}/${demo}.elf"
+                repl="${!dashboard}/${platform}/${demo}/${demo}.repl"
+                # The binary is stored in a separate directory
+                elf="$zephyr_dashboard_base/zephyr/${!version}/${platform}/${demo}/${demo}.elf"
             fi
             # First we replace Jinja templates in the demo-specific input. We'll use it later to fill out the Python template file
             jinja -D platform ${platform} -D platform_original ${platform_original} -D uart_name ${uart_names[j]} -D gpio_led_name ${gpio_led_names[j]} -D software_version "${!version}" -o "$tmp/${platform}_${demo}" ${demo}
