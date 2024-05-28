289094ee765bb06022822f13e468178b2c5f270f
RenodeBot <renode-bot@antmicro.com>
2024-05-28 08:22:28 +0000

    Automatic update of ipynb files
    Signed-off-by: RenodeBot <renode-bot@antmicro.com>

--- boards/s32z2xxdc2_s32z270_rtu1_tensorflow_lite_micro.py
+++ boards/s32z2xxdc2_s32z270_rtu1_tensorflow_lite_micro.py
@@ -77,8 +77,9 @@ asciinema.display_asciicast('output.asciinema')
 
 # %%
 import sys
+from pathlib import Path
 from renode_run import get_default_renode_path
-sys.path.append(get_default_renode_path())
+sys.path.append(str(Path(get_default_renode_path()).parent))
 
 from renode_colab_tools import metrics
 from tools.metrics_analyzer.metrics_parser import MetricsParser
