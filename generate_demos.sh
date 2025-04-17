99bbc30b521c718c53c2c9f9a050b5f76a13e5f7
Adrian Ściepura <asciepura@internships.antmicro.com>
2025-04-17 17:12:23 +0200

    [#75647] Update demo generator

--- generate_demos.sh
+++ generate_demos.sh
@@ -17,8 +17,6 @@ uboot_renode_version="$(curl -s $uboot_dashboard_base/uboot_sim/$uboot_VERSION/l
 uboot_DASHBOARD="$uboot_dashboard_base/uboot_sim/$uboot_VERSION/$uboot_renode_version"
 uboot_DASHBOARD_BUILD="$uboot_dashboard_base/uboot/$uboot_VERSION"
 
-SET_VTOR="cpu0 VectorTableOffset \`sysbus GetSymbolAddress \"_vector_table\"\`"
-
 tmp="$(mktemp --tmpdir -d renodepedia-colabs-XXXXXXXXXX)"
 cleanup() {
     rm -rf "$tmp"
@@ -38,7 +36,7 @@ generate_demos() {
     mkdir "$tmp/$1"
     wget -P "$tmp" "${!dashboard}"/replkit.tar.xz
     # Assumption: no two boards with different architectures will have the same name
-    tar xf "$tmp/replkit.tar.xz" -C "$tmp/$1" --wildcards '*.repl' --transform 's:.*/::g'
+    tar xf "$tmp/replkit.tar.xz" -C "$tmp/$1" --wildcards '*.resc' --transform 's:.*/::g'
     rm "$tmp/replkit.tar.xz"
     for demo in "${!demos}"
     do
@@ -56,25 +54,20 @@ generate_demos() {
             platform_original="${original_names[j]}"
             echo "${demo} ${platform}"
 
-            # Generate a script that halts every CPU except cpu0, if any are present
-            repl_path="$tmp/${1}/$platform-$demo.repl"
-            script="$(grep -Po '^.*(?=:\s+CPU\.)' "$repl_path" | { grep -Fxv cpu0 || :; } | while read -r c; do echo "$c IsHalted true"; done)"
-
             board_path="boards/${platform}"
             repl="${!dashboard}/${platform}/${demo}/${demo}.repl"
             elf="${!build_dashboard}/${platform}/${demo}/${demo}.elf"
+            
+            resc_path="$tmp/${1}/$platform-$demo.resc"
+            script=$(cat $resc_path | sed -e "/machine LoadPlatformDescription/c\machine LoadPlatformDescription @${repl}\nmachine EnableProfiler \$ORIGIN/metrics.dump")
 
             # First we replace Jinja templates in the demo-specific input. We'll use it later to fill out the Python template file
             jinja -D platform ${platform} -D platform_original ${platform_original} -D uart_name ${uart_names[j]} -D gpio_led_name ${gpio_led_names[j]} -D software_version "${!version}" -o "$tmp/${platform}_${demo}" ${demo}
             sample=`cat "${tmp}"/${platform}_${demo}`
-            if [ "$1" = zephyr ] && echo ${dts_chain[j]##*-> } | grep -q 'arm/armv.-m' ; then
-                # For ARM Cortex-M platforms we explicitly initialize VTOR in the script, as the OS build system can place it in various parts of the binary.
-                script="$script${script:+$'\n'}$SET_VTOR"
-            fi
             jinja -D sample_name ${demo} -D sample "${sample}" -D platform ${platform} -D platform_original ${platform_original} -D board_path ${board_path} -D uart_name ${uart_names[j]} -D script "$script" -D software_version "${!version}" -D repl "$repl" -D elf "$elf" -o "${board_path}"_"${demo}".py template.py
         done
     done
 }
 
 generate_demos zephyr
-generate_demos uboot
+#generate_demos uboot
